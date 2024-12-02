a84a13019ed1f1648a686187fb4fb65c4899e291
mohammadzakijafari <zaki.jafari5@gmail.com>
2024-12-03 00:20:46 +0100

    first version animals app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import { useState } from "react"
+import AnimalShow from "./components/AnimalShow";
+import './App.css'
 
-
+// function returns a random element from animal array
 function getRandomAnimal () {
+    // array of animals
     const animalArray = ["bird", "dog", "cow", "cat", "horse", "gator"];
+    // returns a random number from array elements
     return animalArray[Math.floor(Math.random() * animalArray.length)];
 }
 
 function App() {
+    // animals state
     const [animals, setAnimals] = useState([]);
     
+    // adds the random animal element to animals state array
     const handleAddAnimalClick = () => {
+        // adding process to array
         setAnimals([...animals, getRandomAnimal()]);
     }
-    console.log(animals);
+    
+    const renderAnimals = animals.map((animal, index) => {
+        return <AnimalShow type = { animal } key = { index } />
+    });
     return(
-        <div className="">
-            <button className="add_animal_btn" onClick={handleAddAnimalClick}> Add Animal </button>
-            <div className="animal-list"> </div>
+        <div className="app">
+            <h1 className="title"> Click Here to Add Your Animal to the List </h1>
+            <button className="add-animal-btn" onClick={handleAddAnimalClick}> Add Animal </button>
+            <div className="animal-list"> { renderAnimals } </div>
         </div>
     )
 }
 
-export default App;
+export default App
--- src/components/AnimalShow.js
+++ src/components/AnimalShow.js
@@ -1,7 +1,36 @@
-function AnimalShow() {
+import './AnimalShow.css'
+import bird from '../svg/bird.svg';
+import dog from '../svg/dog.svg';
+import cow from '../svg/cow.svg';
+import cat from '../svg/cat.svg';
+import horse from '../svg/horse.svg';
+import gator from '../svg/gator.svg';
+import heart from '../svg/heart.svg';
+
+import { useState } from 'react';
+
+
+const svgImg = {
+    bird,
+    dog,
+    cow,
+    cat,
+    horse,
+    gator,
+}
+
+function AnimalShow({ type }) {
+    // State variable for zoom click
+    const [clicks, setClicks] = useState(0);
+
+    // handling zoom clicks
+    const handleHeartZoomClick = () => {
+        setClicks(clicks + 1);
+    }
     return(
-        <div className="">
-            <h1> Hello World </h1>
+        <div className="animal-show" onClick = { handleHeartZoomClick }>
+            <img alt="animal" src = {svgImg[type]} className='animal' />
+            <img alt='heart' src = {heart} style={{width: 10 + 10 * clicks}} className='heart' />
         </div>
     )
 }
